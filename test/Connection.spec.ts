76d5472062948302758b6ca0b65074d9680b5d10
Maarten Casteels <maarten.casteels@trust1team.com>
2016-07-01 15:20:37 +0200

    Change SINON lib and add additional test for connection

--- test/Connection.spec.ts
+++ test/Connection.spec.ts
@@ -4,14 +4,45 @@
  */
 import { Config, Connection } from '../src/Trust1Team';
 import { expect } from 'chai';
-//import { sinon } from 'sinon';
+import * as sinon from  'sinon';
+import { Promise } from 'es6-promise';
 
 describe('Connection', () => {
     let connection:Connection;
-    //var request = sinon.spy();
+    var server:Sinon.SinonFakeServer;
 
     beforeEach(() => {
         connection = new Connection(new Config());
+        server = sinon.fakeServer.create();
     });
 
+    afterEach(() => {
+        server.restore();
+    });
+
+
+    describe('responding to a generic request', function () {
+        beforeEach(function () {
+            var okResponse = [
+                200,
+                {'Content-type': 'application/json'},
+                '{"hello":"world"}'
+            ];
+
+            server.respondWith('GET', '/hello', okResponse);
+        });
+
+        it('returns correct body', function (done) {
+            var p:Promise<any> = connection.get('/hello');
+            p.then(
+                (result) => {
+                    console.log(result);
+                    //expect(result).to.be.eq();
+                    done();
+                }
+            );
+            server.respond();
+            //server.done();
+        });
+    });
 });
