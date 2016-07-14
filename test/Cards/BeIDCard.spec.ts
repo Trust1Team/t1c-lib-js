8c104097b2f9a89c9bc3dca42e5602654ab042f5
Maarten Casteels <maarten.casteels@trust1team.com>
2016-07-14 09:00:28 +0200

    Add basic test for beid card

--- test/Cards/BeIDCard.spec.ts
+++ test/Cards/BeIDCard.spec.ts
@@ -2,3 +2,16 @@
  * @author Maarten Casteels
  * @since 2016
  */
+import * as sinon from "sinon";
+import {BeIDCard} from "../../src/Trust1Team/Cards/BeIDCard";
+
+describe('BeID Card', () => {
+
+    beforeEach(() => {
+        sinon.stub(BeIDCard.prototype, 'rn').throws();
+    });
+
+    it('should load card information', (done) => {
+        done();
+    });
+});
