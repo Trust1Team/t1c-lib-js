f4e669130bbc6fdec92a7939cb2be245451535f9
michallis <michallis@trust1tream.com>
2016-07-16 23:15:25 +0200

    add tslint

--- src/Trust1Connector.ts
+++ src/Trust1Connector.ts
@@ -9,25 +9,24 @@ import {Config} from "./Trust1Team/Config";
 import {Cards} from "./Trust1Team/Cards";
 
 export class Trust1Connector {
-    private config:Config;
-    private cards:Cards;
+    private config: Config;
+    private cards: Cards;
 
-    constructor(config:Config) {
+    constructor(config: Config) {
         this.config = config || new Config();
         this.cards = new Cards(this.config.connectorUrl);
     }
 
     public checkForConnector(callback) {
-        var p = connection.get('https://localhost:12345/v1/info');
+        let p = connection.get("https://localhost:12345/v1/info");
         p.then((result) => {
             return callback(result);
-        })
+        });
     }
 
-    // Card Types
-
-    // Card Readers
-    public beid():BeIDCard {
+    // card Types
+    // card Readers
+    public beid(): BeIDCard {
         return this.cards.belgiumElectronicID;
     }
 }
