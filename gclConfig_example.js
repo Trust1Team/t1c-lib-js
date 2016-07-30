ab1989a436c20eb6def316d05688fa6062a87794
Michallis Pashidis <michallis@trust1team.com>
2016-07-30 10:52:36 +0200

    add example config

---
+++ gclConfig_example.js
@@ -0,0 +1,9 @@
+exports = gclConfig = {
+    proxy: "https://some.domain:8443/",
+    apikey: "somekey",
+    core_version: "v1.0.0",
+    plugins: [
+        {beid:"v1.0.3"},
+        {emv:"v1.0.3"}
+    ]
+};
