9e64c331eab90804e260524f02533b84d529255f
Michallis Pashidis <michallis@trust1team.com>
2016-07-30 10:49:35 +0200

    udpate interface

--- src/scripts/core/client/Connection.ts
+++ src/scripts/core/client/Connection.ts
@@ -8,8 +8,6 @@ import {Promise} from "es6-promise";
 import * as CoreExceptions from "../exceptions/CoreExceptions";
 
 interface Connection {
-    getPromise(url:string, body?:any):Promise<any>;
-    postPromise(url:string, body?:any):Promise<any>;
     get(url:string, callback:(error:CoreExceptions.RestException, data:any) => void);
     post(url:string, body:any, callback:(error:CoreExceptions.RestException, data:any) => void);
 }
@@ -17,45 +15,6 @@ interface Connection {
 class LocalConnection implements Connection {
 
     constructor() {}
-
-    // using Promises
-    private request(method:string, url:string, body:any):Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            let xmlHttp:XMLHttpRequest = new XMLHttpRequest();
-            xmlHttp.onreadystatechange = () => {
-                if (xmlHttp.readyState === 4) {
-                    if (xmlHttp.status === 200) {
-                        if (xmlHttp.responseText && xmlHttp.responseText.length > 0) {
-                            resolve(JSON.parse(xmlHttp.responseText));
-                        } else {
-                            resolve('');
-                        }
-                    } else {
-                        reject(JSON.parse(xmlHttp.responseText));
-                    }
-                }
-            };
-            xmlHttp.onerror = function (e) {
-                reject(e);
-            };
-            xmlHttp.open(method, url, true); // true for asynchronous
-            if (method === 'POST') {
-                xmlHttp.setRequestHeader('Content-Type', 'application/json');
-            }
-            xmlHttp.send(JSON.stringify(body));
-        });
-    }
-
-    public getPromise(url:string, body?:any):Promise<any> {
-        $.getJSON( url, function( data ) {
-            console.log("jquery data: "+JSON.stringify(data));
-        });
-        return this.request('GET', url, body || '');
-    }
-
-    public postPromise(url:string, body?:any):Promise<any> {
-        return this.request('POST', url, body || '');
-    }
 
     // using Callback
     public get(url:string, callback, queryParams?:any):void{
--- src/scripts/core/services/CoreService.ts
+++ src/scripts/core/services/CoreService.ts
@@ -6,16 +6,66 @@ import * as CoreExceptions from "../exceptions/CoreExceptions";
 
 interface AbstractCore{
     info(callback:(error:CoreExceptions.RestException, data:any) => void):void;
-    types(callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    types(callback:(error:CoreExceptions.RestException, data:any) => void):void; // TODO will dissappear
+    verify(callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    download(callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    activate(apikey:string, callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    init(apikey:string, callback:(error:CoreExceptions.RestException, data:any) => void):void; //TODO complete verify/download/register/verificationpoll
+    update(apikey:string, callback:(error:CoreExceptions.RestException, data:any) => void):void; //triggered by config
+    readers(callback:(error:CoreExceptions.RestException, data:any) => void):void; //will contain card info if card inserted
+    pollReaders(config:any,callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    pollCards(config:any,callback:(error:CoreExceptions.RestException, data:any) => void):void;
 }
 
 const CORE_INFO = "/info";
 const CORE_CARD_TYPES = "/card/gettypes"; // TODO should be on reader level
 
 class CoreService implements AbstractCore{
+    verify(callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    download(callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    activate(apikey:string, callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    init(apikey:string, callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    update(apikey:string, callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    readers(callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    pollReaders(config:any, callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
+
+    pollCards(config:any, callback:(error:CoreExceptions.RestException, data:any)=>void):void {
+    }
     constructor(private url:string) {}
     public info(callback) {connection.get(this.url + CORE_INFO,callback);}
     public types(callback) {connection.get(this.url + CORE_CARD_TYPES,callback);}
+    public readTextFile(file)
+    {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    var allText = rawFile.responseText;
+                    alert(allText);
+                }
+            }
+        };
+        rawFile.send(null);
+    }
 }
+
+
 
 export {AbstractCore,CoreService}
