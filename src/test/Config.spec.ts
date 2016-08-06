d62e5cd8ffbcdd5dbac20f9cc44d08122219bcd7
michallis <michallis@telenet.be>
2016-08-06 14:10:26 +0200

    fix fail for tests

--- src/test/Config.spec.ts
+++ src/test/Config.spec.ts
@@ -3,54 +3,60 @@
  * @since 2016
  */
 import { expect } from "chai";
-import {Config} from "../scripts/core/GCLConfig";
+import {GCLConfig} from "../scripts/core/GCLConfig";
 
 describe('Config', () => {
-    let config:Config;
+    let config:GCLConfig;
 
     beforeEach(() => {
-        config = new Config();
+        config = new GCLConfig();
     });
 
-    describe('#connectorUrl', () => {
+    describe('#gcl-config', () => {
         it('should return the default connector url', () => {
-            var url:string = config.connectorUrl;
-            expect(url).to.be.eq('https://localhost:12345/v1');
+            var url:string = config.gclUrl;
+            expect(url).to.be.eq('https://localhost:10433/v1');
         });
 
         it('should return the custom connector url', () => {
-            var test:string = 'http://abc.be/v1';
-            var myConfig:Config = new Config(test);
-            var url:string = myConfig.connectorUrl;
+            let test = "https://localhost:10080/v2";
+            let myConfig:GCLConfig = new GCLConfig();
+            myConfig.gclUrl = test;
+            var url:string = myConfig.gclUrl;
             expect(url).to.be.eq(test);
         });
 
-        it('should return the default connector url when passing empty string', () => {
-            var test:string = '';
-            var myConfig:Config = new Config(test);
-            var url:string = myConfig.connectorUrl;
-            expect(url).to.be.eq('https://localhost:12345/v1');
+        it('should return the custom ds url', () => {
+            let test = "https://dist.t1t.be/v1";
+            let myConfig:GCLConfig = new GCLConfig();
+            myConfig.dsUrl = test;
+            var dsUrl:string = myConfig.dsUrl;
+            expect(dsUrl).to.be.eq(test);
         });
+
+        it('should return the custom ds url', () => {
+            let test = "somekey";
+            let myConfig:GCLConfig = new GCLConfig();
+            myConfig.apiKey = test;
+            var apikey:string = myConfig.apiKey;
+            expect(apikey).to.be.eq(test);
+        });
+
     });
 
-    describe('#distributionUrl', () => {
-        it('should return the default distribution url', () => {
-            var url:string = config.distributionUrl;
-            expect(url).to.be.eq('https://dist.t1t.be/gcl-ds/v1');
-        });
-
-        it('should return the custom distribution url', () => {
-            var test:string = 'http://abc.be/v1';
-            var myConfig:Config = new Config('', test);
-            var url:string = myConfig.distributionUrl;
-            expect(url).to.be.eq(test);
-        });
-
-        it('should return the default distribution url when passing empty string', () => {
-            var test:string = '';
-            var myConfig:Config = new Config(test);
-            var url:string = myConfig.distributionUrl;
-            expect(url).to.be.eq('https://dist.t1t.be/gcl-ds/v1');
+    describe('#gcl-config-constructor', () => {
+        it('should set the config as constructed', () => {
+            var dsUrl = "https://dist.t1t.be/v1";
+            var gclUrl = "https://localhost:10443/v1";
+            var apikey = "someapikey";
+            var allowAutoUpdate = true;
+            var implicitDownload = false;
+            let myConfig = new GCLConfig(gclUrl,dsUrl,apikey,allowAutoUpdate,implicitDownload);
+            expect(myConfig.dsUrl).to.be.eq(dsUrl);
+            expect(myConfig.gclUrl).to.be.eq(gclUrl);
+            expect(myConfig.apiKey).to.be.eq(apikey);
+            expect(myConfig.allowAutoUpdate).to.be.eq(allowAutoUpdate);
+            expect(myConfig.implicitDownload).to.be.eq(implicitDownload);
         });
     });
 });
