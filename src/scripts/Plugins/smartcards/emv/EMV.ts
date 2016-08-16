e61585c05c0d2d108e1355ee2b04cbb658848b19
michallis <michallis@telenet.be>
2016-08-16 06:33:54 +0200

    add bower implement emv pan

--- src/scripts/Plugins/smartcards/emv/EMV.ts
+++ src/scripts/Plugins/smartcards/emv/EMV.ts
@@ -10,13 +10,19 @@ interface AbstractEMV{
 
 }
 
+const SEPARATOR = "/";
 const PLUGIN_CONTEXT_EMV = "/plugins/emv";
-const EMV_PAN = PLUGIN_CONTEXT_EMV + "/pan";
-const EMV_VERIFY = PLUGIN_CONTEXT_EMV + "/verify-pin";
+const EMV_PAN = "/pan";
+const EMV_VERIFY = "/verify-pin";
 
 class EMV implements AbstractEMV{
     constructor(private url:string,private connection:LocalConnection,private reader_id:string) {this.url = url + PLUGIN_CONTEXT_EMV;}
 
-    pan(callback:(error:CoreExceptions.RestException, data:any)=>void):void {this.connection.get(this.url + EMV_PAN,callback);}
+    // resolves the reader_id in the base URL
+    private resolvedReaderURI():string{
+        return this.url + SEPARATOR + this.reader_id;
+    }
+
+    pan(callback:(error:CoreExceptions.RestException, data:any)=>void):void {this.connection.get(this.resolvedReaderURI() + EMV_PAN,callback);}
 }
 export {AbstractEMV, EMV};
