8df32d33a3218530ec9fbd812dc674fe6915fa29
michallis <michallis@telenet.be>
2016-09-06 09:56:30 +0200

    remove config dep and test ajax

--- src/test/integration/plugings/BEeID.spec.ts
+++ src/test/integration/plugings/BEeID.spec.ts
@@ -2,7 +2,6 @@ import {expect} from "chai";
 import {LocalConnection, LocalAuthConnection,Connection} from "../../../scripts/core/client/Connection";
 import {CoreService} from "../../../scripts/core/services/CoreService";
 import {CardFactory} from "../../../scripts/Plugins/smartcards/CardFactory";
-import {GCLConfig} from "../../../scripts/core/GCLConfig";
 
 describe('Plugin-Belgian eID', () => {
     let gclUnderTest = "https://localhost:10443/v1";
@@ -41,25 +40,11 @@ describe('Plugin-Belgian eID', () => {
         });
 
         it('should return available card readers', (done)=>{
-            $.ajax({
-                url: gclUnderTest+'/',
-                type: 'GET',
-                dataType: 'json',
-                headers: { 'Authorization':('Bearer ' + GCLConfig.Instance.jwt), 'Accept-Language':'en-US' },
-                success: function(successResponse,status,jqXHR) {
-                    console.log(JSON.stringify(successResponse));
-                    done();
-                },
-                error: function(jqXHR,textStatus,errorThrown) {
-                    console.log(JSON.stringify(jqXHR));
-                    done();
-                }
-            });
-/*            coreService.readers(function(err,data){
+            coreService.readers(function(err,data){
                 console.log("Error: "+JSON.stringify(err));
                 console.log("data: "+data);
                 done();
-            });*/
+            });
         });
 
     });
