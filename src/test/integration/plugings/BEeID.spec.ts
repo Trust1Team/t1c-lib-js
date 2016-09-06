8ff2b7c5bec4394ff0e946f2849e946d38bbf509
michallis <michallis@telenet.be>
2016-09-06 08:50:26 +0200

    working test framework, taken into consideration ajax in karma

--- src/test/integration/plugings/BEeID.spec.ts
+++ src/test/integration/plugings/BEeID.spec.ts
@@ -1,29 +1,28 @@
 import {expect} from "chai";
-import {LocalConnection, LocalAuthConnection} from "../../../scripts/core/client/Connection";
+import {LocalConnection, LocalAuthConnection,Connection} from "../../../scripts/core/client/Connection";
 import {CoreService} from "../../../scripts/core/services/CoreService";
 import {CardFactory} from "../../../scripts/Plugins/smartcards/CardFactory";
+import {GCLConfig} from "../../../scripts/core/GCLConfig";
 
 describe('Plugin-Belgian eID', () => {
     let gclUnderTest = "https://localhost:10443/v1";
-    let localConnection:LocalConnection;
     let localAuthConnection:LocalAuthConnection;
     let coreService;
     let cardFactory;
     let beidPlugin;
 
     beforeEach(()=>{
-        localConnection = new LocalConnection();
         localAuthConnection = new LocalAuthConnection();
         coreService = new CoreService(gclUnderTest,localAuthConnection);
-        cardFactory = new CardFactory(gclUnderTest,localConnection)
+        cardFactory = new CardFactory(gclUnderTest,localAuthConnection);
         beidPlugin = cardFactory.createEidBE();
     });
 
     describe('Belgian eID Test Cases', () => {
 
-        it('should verify an existing local connection instance', ()=>{
+/*        it('should verify an existing local connection instance', ()=>{
            expect(localConnection).not.undefined;
-        });
+        });*/
 
         it('should verify an existing local authentication connection instance', ()=>{
             expect(localAuthConnection).not.undefined;
@@ -41,5 +40,29 @@ describe('Plugin-Belgian eID', () => {
             expect(beidPlugin).not.undefined;
         });
 
+        it('should return available card readers', (done)=>{
+            $.ajax({
+                url: gclUnderTest+'/',
+                type: 'GET',
+                dataType: 'json',
+                headers: { 'Authorization':('Bearer ' + GCLConfig.Instance.jwt), 'Accept-Language':'en-US' },
+                success: function(successResponse,status,jqXHR) {
+                    console.log(JSON.stringify(successResponse));
+                    done();
+                },
+                error: function(jqXHR,textStatus,errorThrown) {
+                    console.log(JSON.stringify(jqXHR));
+                    done();
+                }
+            });
+/*            coreService.readers(function(err,data){
+                console.log("Error: "+JSON.stringify(err));
+                console.log("data: "+data);
+                done();
+            });*/
+        });
+
     });
 });
+
+
