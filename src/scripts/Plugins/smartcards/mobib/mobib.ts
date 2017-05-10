8f9f59d50f64f5cd36ae867c17f514b8308dcdd4
Maarten Somers <maarten.somers@gmail.com>
2017-05-10 11:24:01 +0200

    Clean Mobil (WIP)

--- src/scripts/Plugins/smartcards/mobib/mobib.ts
+++ src/scripts/Plugins/smartcards/mobib/mobib.ts
@@ -2,15 +2,41 @@
  * @author Maarten Somers
  * @since 2017
  */
-import {LocalConnection} from "../../../core/client/Connection";
-import * as CoreExceptions from "../../../core/exceptions/CoreExceptions";
+import { LocalConnection } from "../../../core/client/Connection";
+import { RestException } from "../../../core/exceptions/CoreExceptions";
+import { DataResponse, T1CResponse } from "../../../core/service/CoreModel";
 
 interface AbstractMobib {
-    allData(filters:string[],callback:(error:CoreExceptions.RestException, data:any) => void):void;
-    cardIssuing(callback:(error:CoreExceptions.RestException, data:any) => void):void;
-    contracts(callback:(error:CoreExceptions.RestException, data:any) => void):void;
-    picture(callback:(error:CoreExceptions.RestException, data:any) => void):void;
-    status(callback:(error:CoreExceptions.RestException, data:any) => void):void;
+    allData(filters: string[], callback: (error: RestException, data: any) => void): void;
+    cardIssuing(callback: (error: RestException, data: any) => void): void;
+    contracts(callback: (error: RestException, data: any) => void): void;
+    picture(callback: (error: RestException, data: DataResponse) => void): void;
+    status(callback: (error: RestException, data: StatusResponse) => void): void;
+}
+
+interface StatusResponse extends T1CResponse {
+    data: {
+        active: boolean
+    }
+}
+
+interface CardIssuing {
+    card_expiration_date: string
+    card_holder_birth_date: string
+    card_holder_end_date: string
+    card_holder_id: string
+    card_holder_name: string
+    card_holder_start_date:  string
+    card_revalidation_date: string
+    card_type: number
+    company_id: number
+    gender: number
+    language: number
+    version: number
+}
+
+interface CardIssuingResponse extends T1CResponse {
+    data: CardIssuing
 }
 
 const SEPARATOR = "/";
@@ -20,21 +46,19 @@ const MOBIB_CONTRACTS = "/contracts";
 const MOBIB_PHOTO = "/picture";
 const MOBIB_STATUS = "/status";
 
-function createFilter(filters:string[]):any {
-    return { filter: filters.join(',') };
+function createFilter(filters: string[]): { filter: string } {
+    return { filter: filters.join(",") };
 }
 
 
-class Mobib implements AbstractMobib{
+class Mobib implements AbstractMobib {
     // constructor
-    constructor(private url:string,private connection:LocalConnection,private reader_id:string) {this.url = url + PLUGIN_CONTEXT_MOBIB;}
-
-    // resolves the reader_id in the base URL
-    private resolvedReaderURI():string{
-        return this.url + SEPARATOR + this.reader_id;
+    constructor(private url: string, private connection: LocalConnection, private reader_id: string) {
+        this.url = url + PLUGIN_CONTEXT_MOBIB;
     }
 
-    public allData(filters, callback) {
+
+    public allData(filters: string[], callback) {
         if (filters && filters.length > 0) { this.connection.get(this.resolvedReaderURI(), callback, createFilter(filters)); }
         else { this.connection.get(this.resolvedReaderURI(), callback); }
     }
@@ -55,6 +79,12 @@ class Mobib implements AbstractMobib{
     public status(callback) {
         this.connection.get(this.resolvedReaderURI() + MOBIB_STATUS, callback);
     }
+
+
+    // resolves the reader_id in the base URL
+    private resolvedReaderURI():string{
+        return this.url + SEPARATOR + this.reader_id;
+    }
 }
 
-export { AbstractMobib, Mobib }
+export { Mobib };
