35ae12672060e3e2cc018a82a84c3725a02cc741
Maarten Somers <maarten.somers@gmail.com>
2017-05-11 13:58:21 +0200

    Tweak some options

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 var path = require("path");
 module.exports = {
-    entry: './src/scripts/core/GCLLib.ts',
+    entry: "./src/scripts/core/GCLLib.ts",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: 'GCLLib.js',
-        library: ['GCLLib']
+        filename: "GCLLib.js",
+        library: ["GCLLib"]
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     resolve: {
-        extensions: ['.ts', '.js', '.tsx', '.jsx', '']
+        extensions: [".ts", ".js", ".tsx", ".jsx", ""]
     },
     module: {
         loaders: [
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-                loader: 'ts-loader'
+                loader: "ts-loader"
             }
         ]
     }
