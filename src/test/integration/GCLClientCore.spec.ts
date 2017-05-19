facaeee15eb8c17414b076bf58b4c73a4a2795d8
Maarten Somers <maarten.somers@gmail.com>
2017-05-19 12:07:41 +0200

    Really fix tests this time

--- src/test/integration/GCLClientCore.spec.ts
+++ src/test/integration/GCLClientCore.spec.ts
@@ -1,44 +1,44 @@
 ///<reference path="../../../typings/index.d.ts"/>
 import {expect} from "chai";
 import {LocalAuthConnection} from "../../scripts/core/client/Connection";
-import {CoreService} from "../../scripts/core/services/CoreService";
+import {CoreService} from "../../scripts/core/service/CoreService";
 import {GCLConfig} from "../../scripts/core/GCLConfig";
-import {TestUtil} from "../../test-util/TestUtils"
+import {TestUtil} from "../../test-util/TestUtils";
 
-describe('GCLClient', () => {
+describe("GCLClient", () => {
     let gclUnderTest = "https://localhost:10443/v1";
-    let localAuthConnection:LocalAuthConnection;
-    let core:CoreService;
-    let config = new GCLConfig("https://dist.t1t.be/v1","someapikey");
+    let localAuthConnection: LocalAuthConnection;
+    let core: CoreService;
+    let config = new GCLConfig("https://dist.t1t.be/v1", "someapikey");
 
     beforeEach(() => {
         localAuthConnection = new LocalAuthConnection(config);
-        core = new CoreService(gclUnderTest,localAuthConnection, config);
+        core = new CoreService(gclUnderTest, localAuthConnection);
     });
 
-    describe('GCL Core Service', () => {
+    describe("GCL Core Service", () => {
 
-        it('should verify that a connection has been instantiated', () => {
+        it("should verify that a connection has been instantiated", () => {
             expect(localAuthConnection).not.undefined;
         });
 
-        it('should verify that a core service instance is available', () => {
+        it("should verify that a core service instance is available", () => {
             expect(core).not.undefined;
         });
 
-        it('should verify that the URL has been set correctly', () => {
+        it("should verify that the URL has been set correctly", () => {
             expect(core.getUrl()).to.equals(gclUnderTest);
         });
 
-        it('should return information about the client browser', (done) => {
-            core.infoBrowser((err,data) => {
+        it("should return information about the client browser", (done) => {
+            core.infoBrowser((err, data) => {
                 expect(err).to.be.null;
                 expect(data).exist;
                 done();
             });
         });
 
-        // it('should return info about the GCL installation', (done) => {
+        // it("should return info about the GCL installation", (done) => {
         //     core.info((err, data) => {
         //         TestUtil.asyncCheck(done, () => {
         //             expect(err).to.be.null;
@@ -47,7 +47,7 @@ describe('GCLClient', () => {
         //     });
         // });
 
-        // it('should return available card readers', (done) => {
+        // it("should return available card readers", (done) => {
         //     core.readers((err,data) => {
         //         TestUtil.asyncCheck(done, () => {
         //             expect(err).to.be.null;
@@ -56,7 +56,7 @@ describe('GCLClient', () => {
         //     });
         // });
 
-        // it('should return readers with available cards', (done) => {
+        // it("should return readers with available cards", (done) => {
         //     core.readersCardAvailable((err,data) => {
         //         TestUtil.asyncCheck(done, () => {
         //             expect(err).to.be.null;
@@ -65,7 +65,7 @@ describe('GCLClient', () => {
         //     });
         // });
 
-        // it('should return readers without available cards', (done) => {
+        // it("should return readers without available cards", (done) => {
         //     core.readersCardsUnavailable((err,data) => {
         //         TestUtil.asyncCheck(done, () => {
         //             expect(err).to.be.null;
@@ -74,7 +74,7 @@ describe('GCLClient', () => {
         //     });
         // });
 
-        // it('should return a list of available plugins', (done) => {
+        // it("should return a list of available plugins", (done) => {
         //     core.plugins((err, data) => {
         //         TestUtil.asyncCheck(done, () => {
         //             expect(err).to.be.null;
@@ -83,9 +83,9 @@ describe('GCLClient', () => {
         //     });
         // });
 
-        it('should return the T1C-Lib version', () => {
-            expect(core.version()).to.be.a('string');
-            expect(core.version()).to.equal('%%GULP_INJECT_VERSION%%');
+        it("should return the T1C-Lib version", () => {
+            expect(core.version()).to.be.a("string");
+            expect(core.version()).to.equal("%%GULP_INJECT_VERSION%%");
         });
     });
 });
