0a5a7092758aabf2efb3536bb9b109301ef832e8
Maarten Somers <maarten.somers@gmail.com>
2017-06-20 17:14:08 +0200

    Add PKCS11 implementation

--- src/scripts/plugins/pkcs11/pkcs11Model.ts
+++ src/scripts/plugins/pkcs11/pkcs11Model.ts
@@ -1,3 +1,14 @@
 /**
- * Created by maarten on 6/20/17.
+ * @author Maarten Somers
  */
+
+import { OptionalPin } from "../smartcards/Card";
+import { RestException } from "../../core/exceptions/CoreExceptions";
+import { CertificatesResponse } from "../../core/service/CoreModel";
+
+export { AbstractPkcs11 };
+
+interface AbstractPkcs11 {
+    certificates(body: OptionalPin,
+                 callback?: (error: RestException, data: CertificatesResponse) => void): Promise<CertificatesResponse>;
+}
