f3c7a017a610361413f4f3959e365be6982e7710
Maarten Somers <maarten.somers@gmail.com>
2017-09-01 15:05:29 +0200

    Make Belfius container extend the remote loading container

--- src/scripts/plugins/PluginFactory.ts
+++ src/scripts/plugins/PluginFactory.ts
@@ -99,6 +99,6 @@ export class PluginFactory implements AbstractFactory {
     }
 
     public createBelfius(reader_id?: string): AbstractBelfius {
-        return new Belfius(new RemoteLoading(this.url, CONTAINER_REMOTE_LOADING, this.connection, reader_id));
+        return new Belfius(this.url, CONTAINER_REMOTE_LOADING, this.connection, reader_id);
     }
 }
