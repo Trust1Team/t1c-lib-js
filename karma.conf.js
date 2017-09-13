01e8ee21693b7eca45e49d693cc62cbd24928b2a
Maarten Somers <maarten.somers@gmail.com>
2017-09-13 11:28:36 +0200

    Add HTML test report

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,19 @@ module.exports = function (config) {
             module: webpackConfig.module,
             resolve: webpackConfig.resolve
         },
-        reporters: ['progress'],
+
+        htmlReporter: {
+            outputFile: 'reports/unit-tests.html',
+
+            // Optional
+            pageTitle: 'Trust1Connector JS Library',
+            subPageTitle: 'Unit Tests',
+            groupSuites: true,
+            useCompactStyle: true,
+            useLegacyStyle: false
+        },
+
+        reporters: ['progress', 'html'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_DEBUG,
