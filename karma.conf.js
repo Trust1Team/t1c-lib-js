2a9a79326c328a31d5ea1ca543795fd8de83c3f4
Maarten <maarten.somers@gmail.com>
2017-10-25 14:41:56 +0200

    Fix Karma test runner issues

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,9 @@ module.exports = function (config) {
     config.set({
         basePath: '',
         frameworks: ['mocha', 'chai', 'sinon'],
+        plugins: [require('karma-htmlfile-reporter'), require('progress'), require('karma-mocha'),
+                  require('karma-phantomjs-launcher'), require('karma-chai'), require('karma-sinon'),
+                  require('karma-teamcity-reporter'), require('karma-webpack')],
         files: [
             'node_modules/es6-promise/dist/es6-promise.js',
             'src/test/**/*.ts'
