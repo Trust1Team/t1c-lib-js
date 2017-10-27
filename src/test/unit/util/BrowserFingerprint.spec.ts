19c52e4bcc677e1883d89cc9a2365909e44cd0ac
Maarten <maarten.somers@gmail.com>
2017-10-27 17:27:06 +0200

    T1C-817: Add tests for browser fingerprinting

---
+++ src/test/unit/util/BrowserFingerprint.spec.ts
@@ -0,0 +1,43 @@
+/**
+ * @author Maarten Somers
+ * @since 2017
+ */
+
+import { expect } from 'chai';
+import { BrowserFingerprint } from '../../../scripts/util/BrowserFingerprint';
+import * as ls from 'local-storage';
+
+
+describe('Browser Fingerprint Utility', () => {
+
+    describe('can set and retrieve browser fingerprint token', function () {
+        beforeEach(() => {
+            ls.clear();
+        });
+
+        it('can generate a valid token', () => {
+            let token = BrowserFingerprint.get();
+            expect(token).to.be.a('string');
+        });
+
+        it('returns the same token for each call after generating one', () => {
+            let token = BrowserFingerprint.get();
+            let newToken = BrowserFingerprint.get();
+
+            expect(token).to.be.a('string');
+            expect(newToken).to.be.a('string');
+            expect(token).to.eq(newToken);
+        });
+
+        it('adds a new token if the previous one disappears for whatever reason', () => {
+            let token = BrowserFingerprint.get();
+            ls.remove(BrowserFingerprint.BROWSER_AUTH_TOKEN_LOCATION);
+            let newToken = BrowserFingerprint.get();
+
+            expect(token).to.be.a('string');
+            expect(newToken).to.be.a('string');
+            expect(token).to.not.eq(newToken);
+        })
+    });
+
+});
