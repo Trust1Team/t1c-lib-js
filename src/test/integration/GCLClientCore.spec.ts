b37cee20f56dfa308dcd583e36d2480f155cdead
Maarten <maarten.somers@gmail.com>
2017-10-27 09:22:49 +0200

    T1C-817 Fix Tests

--- src/test/integration/GCLClientCore.spec.ts
+++ src/test/integration/GCLClientCore.spec.ts
@@ -1,35 +1,35 @@
-///<reference path="../../../typings/index.d.ts"/>
-import {expect} from "chai";
-import {LocalAuthConnection} from "../../scripts/core/client/Connection";
-import {CoreService} from "../../scripts/core/service/CoreService";
-import {GCLConfig} from "../../scripts/core/GCLConfig";
+///<reference path='../../../typings/index.d.ts'/>
+import {expect} from 'chai';
+import {LocalAuthConnection} from '../../scripts/core/client/Connection';
+import {CoreService} from '../../scripts/core/service/CoreService';
+import {GCLConfig} from '../../scripts/core/GCLConfig';
 
-describe("GCLClient", () => {
-    let gclUnderTest = "https://localhost:10443/v1";
+describe('GCLClient', () => {
+    let gclUnderTest = 'https://localhost:10443/v1';
     let localAuthConnection: LocalAuthConnection;
     let core: CoreService;
-    let config = new GCLConfig("https://dist.t1t.be/v1", "someapikey");
+    let config = new GCLConfig('https://dist.t1t.be/v1', 'someapikey');
 
     beforeEach(() => {
         localAuthConnection = new LocalAuthConnection(config);
         core = new CoreService(gclUnderTest, localAuthConnection);
     });
 
-    describe("GCL Core Service", () => {
+    describe('GCL Core Service', () => {
 
-        it("should verify that a connection has been instantiated", () => {
+        it('should verify that a connection has been instantiated', () => {
             expect(localAuthConnection).not.undefined;
         });
 
-        it("should verify that a core service instance is available", () => {
+        it('should verify that a core service instance is available', () => {
             expect(core).not.undefined;
         });
 
-        it("should verify that the URL has been set correctly", () => {
+        it('should verify that the URL has been set correctly', () => {
             expect(core.getUrl()).to.equals(gclUnderTest);
         });
 
-        it("should return information about the client browser", (done) => {
+        it('should return information about the client browser', (done) => {
             core.infoBrowser((err, data) => {
                 expect(err).to.be.null;
                 expect(data).exist;
@@ -37,9 +37,11 @@ describe("GCLClient", () => {
             });
         });
 
-        it("should return the T1C-Lib version", () => {
-            expect(core.version()).to.be.a("string");
-            expect(core.version()).to.equal("%%GULP_INJECT_VERSION%%");
+        it('should return the T1C-Lib version', () => {
+            return core.version().then(version => {
+                expect(version).to.be.a('string');
+                expect(version).to.equal('%%GULP_INJECT_VERSION%%');
+            });
         });
     });
 });
