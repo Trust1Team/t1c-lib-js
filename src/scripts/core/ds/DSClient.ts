b3b12766d865275e91cab3680c2da9a5b48c2b4c
Maarten <maarten.somers@gmail.com>
2017-11-15 16:02:37 +0100

    T1C-862 Fix initialization issues in managed context

--- src/scripts/core/ds/DSClient.ts
+++ src/scripts/core/ds/DSClient.ts
@@ -2,27 +2,27 @@
  * @author Michallis Pashidis
  * @author Maarten Somers
  */
-import { Connection } from "../client/Connection";
-import { GCLConfig } from "../GCLConfig";
-import * as CoreExceptions from "../exceptions/CoreExceptions";
-import * as _ from "lodash";
-import { BrowserInfo } from "../service/CoreModel";
+import { Connection } from '../client/Connection';
+import { GCLConfig } from '../GCLConfig';
+import * as CoreExceptions from '../exceptions/CoreExceptions';
+import * as _ from 'lodash';
+import { BrowserInfo } from '../service/CoreModel';
 import { AbstractDSClient, DeviceResponse, DownloadLinkResponse,
-    DSInfoResponse, DSPlatformInfo, DSPubKeyResponse, JWTResponse } from "./DSClientModel";
-import { Promise } from "es6-promise";
+    DSInfoResponse, DSPlatformInfo, DSPubKeyResponse, JWTResponse } from './DSClientModel';
+import { Promise } from 'es6-promise';
 
 export { DSClient };
 
 
-const SEPARATOR = "/";
-const QP_APIKEY = "?apikey=";
-const SECURITY = "/security";
-const SYS_INFO = "/system/status";
-const SECURITY_JWT_ISSUE = SECURITY + "/jwt/issue";
-const SECURITY_JWT_REFRESH = SECURITY + "/jwt/refresh";
-const DOWNLOAD = "/download/gcl";
-const PUB_KEY = SECURITY + "/keys/public";
-const DEVICE = "/devices";
+const SEPARATOR = '/';
+const QP_APIKEY = '?apikey=';
+const SECURITY = '/security';
+const SYS_INFO = '/system/status';
+const SECURITY_JWT_ISSUE = SECURITY + '/jwt/issue';
+const SECURITY_JWT_REFRESH = SECURITY + '/jwt/refresh';
+const DOWNLOAD = '/download/gcl';
+const PUB_KEY = SECURITY + '/keys/public';
+const DEVICE = '/devices';
 
 
 class DSClient implements AbstractDSClient {
@@ -70,13 +70,13 @@ class DSClient implements AbstractDSClient {
         if (actualJWT) {
             return this.connection.post(this.url, SECURITY_JWT_REFRESH, { originalJWT: actualJWT }, undefined, callback);
         } else {
-            let error = { code: "500", description: "No JWT available", status: 412 };
+            let error = { code: '500', description: 'No JWT available', status: 412 };
             if (callback) { return callback(error, null); }
             else { return Promise.reject(error); }
         }
     }
 
-    public getPubKey(callback?: (error: CoreExceptions.RestException, data: DSPubKeyResponse) => void): void | Promise<DSPubKeyResponse> {
+    public getPubKey(callback?: (error: CoreExceptions.RestException, data: DSPubKeyResponse) => void): Promise<DSPubKeyResponse> {
         return this.connection.get(this.url, PUB_KEY, undefined, callback);
     }
 
@@ -107,14 +107,14 @@ class DSClient implements AbstractDSClient {
     }
 
     public register(info: DSPlatformInfo, device_id: string,
-                    callback?: (error: CoreExceptions.RestException, data: JWTResponse) => void): void | Promise<JWTResponse> {
-        let req = _.merge({ uuid: device_id, version: info.core_version }, _.omit(info, "core_version"));
+                    callback?: (error: CoreExceptions.RestException, data: JWTResponse) => void): Promise<JWTResponse> {
+        let req = _.merge({ uuid: device_id, version: info.core_version }, _.omit(info, 'core_version'));
         return this.connection.put(this.url, DEVICE + SEPARATOR + device_id, req, undefined, callback);
     }
 
     public sync(info: DSPlatformInfo, device_id: string,
-                callback?: (error: CoreExceptions.RestException, data: JWTResponse) => void): void | Promise<JWTResponse> {
-        let req = _.merge({ uuid: device_id, version: info.core_version }, _.omit(info, "core_version"));
+                callback?: (error: CoreExceptions.RestException, data: JWTResponse) => void): Promise<JWTResponse> {
+        let req = _.merge({ uuid: device_id, version: info.core_version }, _.omit(info, 'core_version'));
         return this.connection.post(this.url, DEVICE + SEPARATOR + device_id, req, undefined, callback);
     }
 
