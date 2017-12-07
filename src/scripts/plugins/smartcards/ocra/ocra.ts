3e67826e9fb0ce33d2a0b3f5a7cdbba0dbcd4bc4
Maarten <maarten.somers@gmail.com>
2017-12-07 17:31:13 +0100

    Fix counter call

--- src/scripts/plugins/smartcards/ocra/ocra.ts
+++ src/scripts/plugins/smartcards/ocra/ocra.ts
@@ -2,22 +2,22 @@
  * @author Maarten Somers
  * @since 2017
  */
-import { GenericPinCard, OptionalPin } from "../Card";
-import { DataResponse } from "../../../core/service/CoreModel";
-import { RestException } from "../../../core/exceptions/CoreExceptions";
-import { AbstractOcra, ChallengeData, ReadCounterResponse } from "./ocraModel";
-import { PinEnforcer } from "../../../util/PinEnforcer";
-import { Promise } from "es6-promise";
+import { GenericPinCard, OptionalPin } from '../Card';
+import { DataResponse } from '../../../core/service/CoreModel';
+import { RestException } from '../../../core/exceptions/CoreExceptions';
+import { AbstractOcra, ChallengeData, ReadCounterResponse } from './ocraModel';
+import { PinEnforcer } from '../../../util/PinEnforcer';
+import { Promise } from 'es6-promise';
 
 export { Ocra };
 
 
 class Ocra extends GenericPinCard implements AbstractOcra {
-    static CHALLENGE = "/challenge";
-    static READ_COUNTER = "/read-counter";
+    static CHALLENGE = '/challenge';
+    static READ_COUNTER = '/counter';
 
     public challenge(body: ChallengeData, callback?: (error: RestException, data: DataResponse) => void): Promise<DataResponse> {
-        if (callback && typeof callback === "function") {
+        if (callback && typeof callback === 'function') {
             PinEnforcer.check(this.connection, this.baseUrl, this.reader_id, body.pin).then(() => {
                 return this.connection.post(this.baseUrl, this.containerSuffix(Ocra.CHALLENGE), body, undefined, callback);
             }, error => {
@@ -34,16 +34,16 @@ class Ocra extends GenericPinCard implements AbstractOcra {
 
     public readCounter(body: OptionalPin,
                        callback?: (error: RestException, data: ReadCounterResponse) => void): Promise<ReadCounterResponse> {
-        if (callback && typeof callback === "function") {
+        if (callback && typeof callback === 'function') {
             PinEnforcer.check(this.connection, this.baseUrl, this.reader_id, body.pin).then(() => {
-                return this.connection.post(this.baseUrl, this.containerSuffix(Ocra.READ_COUNTER), body, undefined, callback);
+                return this.connection.get(this.baseUrl, this.containerSuffix(Ocra.READ_COUNTER), { pin: body.pin}, callback);
             }, error => {
                 return callback(error, null);
             });
         } else {
             return new Promise((resolve, reject) => {
                 PinEnforcer.check(this.connection, this.baseUrl, this.reader_id, body.pin).then(() => {
-                    resolve(this.connection.post(this.baseUrl, this.containerSuffix(Ocra.READ_COUNTER), body, undefined));
+                    resolve(this.connection.get(this.baseUrl, this.containerSuffix(Ocra.READ_COUNTER), { pin: body.pin}, undefined));
                 }, error => { reject(error); });
             });
         }
