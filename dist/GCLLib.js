cf812e6595ab649c609c2b601369788cc2be1b27
Maarten <maarten.somers@gmail.com>
2017-12-18 11:41:20 +0100

    Bump version, update Readme, npm publish

--- dist/GCLLib.js
+++ dist/GCLLib.js
@@ -28232,7 +28232,7 @@ var GCLLib =
 	    CoreService.prototype.infoBrowserSync = function () { return CoreService.platformInfo(); };
 	    CoreService.prototype.getUrl = function () { return this.url; };
 	    CoreService.prototype.version = function () {
-	        return es6_promise_1.Promise.resolve('v1.7.0');
+	        return es6_promise_1.Promise.resolve('v1.8.0');
 	    };
 	    return CoreService;
 	}());
