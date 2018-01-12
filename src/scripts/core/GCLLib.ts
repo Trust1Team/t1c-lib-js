e303347a898960c06f30772cc9dff13b9fb895ac
Maarten <maarten.somers@gmail.com>
2018-01-12 12:29:21 +0100

    Merge branch 'release/v1.5.2'

--- src/scripts/core/GCLLib.ts
+++ src/scripts/core/GCLLib.ts
@@ -169,7 +169,7 @@ class GCLClient {
     // get instance for Belfius
     public belfius = (reader_id: string): AbstractBelfius => { return this.pluginFactory.createBelfius(reader_id); };
     // get instance for File Exchange
-    public fileExchange = (): AbstractFileExchange => { return this.pluginFactory.createFileExchange(); };
+    // public fileExchange = (): AbstractFileExchange => { return this.pluginFactory.createFileExchange(); };
 
     // generic methods
     public containerFor(readerId: string, callback?: (error: RestException, data: DataResponse) => void) {
