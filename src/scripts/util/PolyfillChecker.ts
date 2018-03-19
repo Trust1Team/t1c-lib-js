bb6903e70060fd83e496cb33657d345548c8360e
Maarten <maarten.somers@gmail.com>
2018-03-19 18:03:30 +0100

    T1C-1062: Expand array polyfill

--- src/scripts/util/PolyfillChecker.ts
+++ src/scripts/util/PolyfillChecker.ts
@@ -24,6 +24,49 @@ class PolyfillChecker {
             // not found, load array.from polyfill (core-js)
             console.log('polyfilling Array');
             require('core-js/es6/array');
+            require('core-js/fn/array/from');
+            require('core-js/fn/array/of');
+            require('core-js/fn/array/is-array');
+            require('core-js/fn/array/iterator');
+            require('core-js/fn/array/copy-within');
+            require('core-js/fn/array/fill');
+            require('core-js/fn/array/find');
+            require('core-js/fn/array/find-index');
+            require('core-js/fn/array/values');
+            require('core-js/fn/array/keys');
+            require('core-js/fn/array/entries');
+            require('core-js/fn/array/slice');
+            require('core-js/fn/array/join');
+            require('core-js/fn/array/index-of');
+            require('core-js/fn/array/last-index-of');
+            require('core-js/fn/array/every');
+            require('core-js/fn/array/some');
+            require('core-js/fn/array/for-each');
+            require('core-js/fn/array/map');
+            require('core-js/fn/array/filter');
+            require('core-js/fn/array/reduce');
+            require('core-js/fn/array/reduce-right');
+            require('core-js/fn/array/sort');
+            require('core-js/fn/array/virtual/iterator');
+            require('core-js/fn/array/virtual/copy-within');
+            require('core-js/fn/array/virtual/fill');
+            require('core-js/fn/array/virtual/find');
+            require('core-js/fn/array/virtual/find-index');
+            require('core-js/fn/array/virtual/values');
+            require('core-js/fn/array/virtual/keys');
+            require('core-js/fn/array/virtual/entries');
+            require('core-js/fn/array/virtual/slice');
+            require('core-js/fn/array/virtual/join');
+            require('core-js/fn/array/virtual/index-of');
+            require('core-js/fn/array/virtual/last-index-of');
+            require('core-js/fn/array/virtual/every');
+            require('core-js/fn/array/virtual/some');
+            require('core-js/fn/array/virtual/for-each');
+            require('core-js/fn/array/virtual/map');
+            require('core-js/fn/array/virtual/filter');
+            require('core-js/fn/array/virtual/reduce');
+            require('core-js/fn/array/virtual/reduce-right');
+            require('core-js/fn/array/virtual/sort');
         }
 
         // check Symbol
