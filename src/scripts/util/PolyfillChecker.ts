a48f1c8cca0d2df061e629880946c78506ff4b41
Maarten <maarten.somers@gmail.com>
2018-03-19 18:35:51 +0100

    T1C-1062: Add typed to polyfills required for array

--- src/scripts/util/PolyfillChecker.ts
+++ src/scripts/util/PolyfillChecker.ts
@@ -24,22 +24,7 @@ class PolyfillChecker {
             // not found, load array.from polyfill (core-js)
             console.log('polyfilling Array');
             require('core-js/es6/array');
-            require('core-js/es6/date');
-            require('core-js/es6/function');
-            require('core-js/es6/index');
-            require('core-js/es6/map');
-            require('core-js/es6/math');
-            require('core-js/es6/number');
-            require('core-js/es6/object');
-            require('core-js/es6/parse-float');
-            require('core-js/es6/parse-int');
-            require('core-js/es6/reflect');
-            require('core-js/es6/regexp');
-            require('core-js/es6/set');
-            require('core-js/es6/string');
             require('core-js/es6/typed');
-            require('core-js/es6/weak-map');
-            require('core-js/es6/weak-set');
         }
 
         // check Symbol
