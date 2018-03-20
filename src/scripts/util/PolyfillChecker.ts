92afc73f9119d186d35765d20d5ced286a65d8b1
Maarten <maarten.somers@gmail.com>
2018-03-20 09:50:54 +0100

    Attempt to fix Edge

--- src/scripts/util/PolyfillChecker.ts
+++ src/scripts/util/PolyfillChecker.ts
@@ -32,5 +32,22 @@ class PolyfillChecker {
             console.log('polyfilling Symbol');
             require('core-js/es6/symbol');
         }
+
+
+        require('core-js/es6/date');
+        require('core-js/es6/function');
+        require('core-js/es6/index');
+        require('core-js/es6/map');
+        require('core-js/es6/math');
+        require('core-js/es6/number');
+        require('core-js/es6/object');
+        require('core-js/es6/parse-float');
+        require('core-js/es6/parse-int');
+        require('core-js/es6/relect');
+        require('core-js/es6/regexp');
+        require('core-js/es6/set');
+        require('core-js/es6/string');
+        require('core-js/es6/weak-map');
+        require('core-js/es6/weak-set');
     }
 }
