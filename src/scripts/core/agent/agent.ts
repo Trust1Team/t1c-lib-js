921a5c4685e6dea40fffca3b6180d5648a281ca6
Maarten <maarten.somers@gmail.com>
2018-04-19 14:14:51 +0200

    Switch agent to localAuthConnection because it does not need to send relay state

--- src/scripts/core/agent/agent.ts
+++ src/scripts/core/agent/agent.ts
@@ -3,7 +3,7 @@
  */
 import { RestException } from '../exceptions/CoreExceptions';
 import { AbstractAgent, AgentResponse } from './agentModel';
-import { LocalConnection } from '../client/Connection';
+import { LocalAuthConnection } from '../client/Connection';
 
 
 export { AgentClient };
@@ -15,7 +15,7 @@ class AgentClient implements AbstractAgent {
     static AGENT_PATH = '/agent';
 
     // constructor
-    constructor(private url: string, private connection: LocalConnection) {}
+    constructor(private url: string, private connection: LocalAuthConnection) {}
 
     public static urlPrefix(port: number) {
         return AgentClient.AGENT_PATH + '/' + port;
