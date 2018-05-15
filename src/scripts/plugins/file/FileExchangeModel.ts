cb7be65ae51c8d1c8b2f193b248b8364fddaa86d
Michallis Pashidis <michallis@trust1team.com>
2018-05-15 14:42:22 +0200

    init interface

--- src/scripts/plugins/file/FileExchangeModel.ts
+++ src/scripts/plugins/file/FileExchangeModel.ts
@@ -15,13 +15,47 @@ interface AbstractFileExchange {
     downloadFile(path: string, file: ArrayBuffer, fileName: string): Promise<DataResponse>;
     uploadFile(path: string): Promise<ArrayBuffer>;
 
-    getFolderMappings(): Any;
-    getFilesByType(): Any;
-    createFileMapping(): Any;
-    updateFileMapping(): Any;
-    deleteFileMapping(): Any;
-    readFileMapping(): Any;
-    getSupportedFileTypes(): Any;
+    // access rights!!!
+
+    // generic
+    getSupportedFileExtensions(): Any;
+
+    // shows a full list of types related to their mapping (optional mapping)
+    listTypes(): Any;
+    // returns access rights, and if exists
+    listType(type: String): Any;
+    // shows a type and its optional mapping
+    listTypeMap(type: String): Any;
+    // shows all files for a certain mapping or return no files for mapping, or mapping not defined
+    listTypeMapFiles(type: String): Any;
+    // list all files persisted in mapped type folders
+    listFiles(): Any;
+
+    // verifies if the type exists and is mapped
+    existsType(type: String): Any;
+    existsFile(type: String, recursive?: Boolean): Any;
+
+    // create directory -
+    createDirectory(type: String)
+
+
+    // copy operations
+
+    // move operations
+
+
+    // search
+
+    // absolute path vs relative path
+
+    // create a new type and trigger user to choose folder, error handler if no folder chosen by user
+    createTypeMap(type: String, initVal?: String): Any;
+    // implicit => trigger user to choose file in file chooser
+    updateTypeMap(type: String, implicit?: Boolean, initVal?: String): Any;
+    // delete type from mapping (but maintain directory and files)
+    deleteTypeMap(type: String)
+
+
 }
 
 
@@ -41,4 +75,13 @@ class FileListResponse extends T1CResponse {
     constructor(public data: File[], public success: boolean) {
         super(success, data);
     }
+}
+
+enum AccessMode {
+    READ, WRITE, EXECUTE
+}
+
+enum TypeStatus {
+    MAPPED,
+    UNMAPPED
 }
