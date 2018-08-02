d0ebb5387028ca8521ca2ff9ee7cc9e41d9fe9d6
Gilles <platteeuw.gilles@gmail.com>
2018-08-02 12:21:17 +0200

    added jest and creaded admin.spec

--- lib-esm/scripts/util/ClientService.d.ts
+++
@@ -1,6 +0,0 @@
-import { GCLClient } from '../core/GCLLib';
-export declare class ClientService {
-    private static client;
-    static getClient(): GCLClient;
-    static setClient(newClient: GCLClient): void;
-}
