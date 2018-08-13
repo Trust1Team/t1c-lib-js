135fc2d5d242007473f75e58a704816069ca648e
Gilles <platteeuw.gilles@gmail.com>
2018-08-13 14:26:25 +0200

    added polyfills

--- src/scripts/util/Polyfills.ts
+++ src/scripts/util/Polyfills.ts
@@ -52,5 +52,87 @@ export class Polyfills {
                 return this.substring(this_len - search.length, this_len) === search;
             };
         }
+
+        // filter polyfill
+        if (!Array.prototype.filter) {
+            Array.prototype.filter = function(fun) {
+                'use strict';
+                if (this === void 0 || this === null) {
+                    throw new TypeError();
+                }
+                let t = Object(this);
+                let len = t.length >>> 0;
+                if (typeof fun !== 'function') {
+                    throw new TypeError();
+                }
+                let res = [];
+                let thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+                for (let i = 0; i < len; i++) {
+                    if (i in t) {
+                        let val = t[i];
+                        if (fun.call(thisArg, val, i, t)) {
+                            res.push(val);
+                        }
+                    }
+                }
+                return res;
+            };
+        }
+
+        // array.find polyfill
+        if (!Array.prototype.find) {
+            Object.defineProperty(Array.prototype, 'find', {
+                value(predicate) {
+                    if (this == null) {
+                        throw new TypeError('"this" is null or not defined');
+                    }
+
+                    let o = Object(this);
+                    let len = o.length >>> 0;
+                    if (typeof predicate !== 'function') {
+                        throw new TypeError('predicate must be a function');
+                    }
+                    let thisArg = arguments[1];
+                    let k = 0;
+                    while (k < len) {
+                        let kValue = o[k];
+                        if (predicate.call(thisArg, kValue, k, o)) {
+                            return kValue;
+                        }
+                        k++;
+                    }
+                    return undefined;
+                },
+                configurable: true,
+                writable: true
+            });
+        }
+
+        // foreach polyfill
+        if (!Array.prototype.forEach) {
+            Array.prototype.forEach = function(callback) {
+                let T, k;
+                if (this == null) {
+                    throw new TypeError('this is null or not defined');
+                }
+                let O = Object(this);
+                let len = O.length >>> 0;
+                if (typeof callback !== 'function') {
+                    throw new TypeError(callback + ' is not a function');
+                }
+                if (arguments.length > 1) {
+                    T = arguments[1];
+                }
+                k = 0;
+                while (k < len) {
+                    let kValue;
+                    if (k in O) {
+                        kValue = O[k];
+                        callback.call(T, kValue, k, O);
+                    }
+                    k++;
+                }
+            };
+        }
     }
 }
