9875825e3e2e8c993f204738d4bcb3bdad0a12bc
Gilles <platteeuw.gilles@gmail.com>
2018-08-14 17:42:15 +0200

    implemented generate certificate request method

--- src/scripts/plugins/java-key-tool/JavaKeyToolModel.ts
+++ src/scripts/plugins/java-key-tool/JavaKeyToolModel.ts
@@ -9,8 +9,39 @@ import {T1CLibException} from '../../core/exceptions/CoreExceptions';
 
 export interface AbstractJavaKeyTool {
     generateKeyPair(body: GenerateKeyPairData, callback?: (error: T1CLibException, data: GenerateKeyPairResponse) => void): Promise<DataResponse>;
+    GenerateCertificateRequest(body: CSRData, callback?: (error: T1CLibException, data: CSRResponse) => void): Promise<DataResponse>;
 }
 
+//  certificate Signing Request (CSR) data
+export class CSRData {
+    constructor(
+        public entity: string,
+        public type: string,
+        public keystore: string,
+        public alias?: string,
+        public sigalg?: string,
+        public file?: string,
+        public keypass?: string,
+        public dname?: string,
+        public storepass?: string,
+        public storetype?: string,
+        public providername?: string,
+        public providerclass?: string,
+        public providerarg?: string,
+        public providerpath?: string
+    ) {}
+}
+
+//  certificate Signing Request (CSR) response
+export class CSRResponse extends T1CResponse {
+    constructor(public data: CSRResponseData , public success: boolean) {
+        super(success, data);
+    }
+}
+
+export class CSRResponseData {
+    constructor(public base64: string, public path?: string) {}
+}
 
 export class GenerateKeyPairData {
     constructor(
