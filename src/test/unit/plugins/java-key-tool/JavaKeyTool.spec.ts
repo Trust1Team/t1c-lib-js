0310e603cda1eb4a2b12bf66650b944df3d56fc9
Gilles <platteeuw.gilles@gmail.com>
2018-08-16 10:56:27 +0200

    added test

--- src/test/unit/plugins/java-key-tool/JavaKeyTool.spec.ts
+++ src/test/unit/plugins/java-key-tool/JavaKeyTool.spec.ts
@@ -75,4 +75,25 @@ describe('Java key tool', () => {
             // console.log(err)
         });
     });
+
+    test('Import certificate', () => {
+        beforeEach(() => {
+            mock.onPost('containers/java-keytool-v1-0-0/importcert', {entity: 't1t', keystore: 't1t.jks', type: 'test'}).reply(200, {
+                data: true,
+                success: true
+            });
+        })
+        const body = {
+            entity: 't1t',
+            keystore: 't1t.jks',
+            type: 'test'
+        };
+
+        jks.ImportCertificate(body).then(res => {
+            expect(res).toHaveProperty('success', true);
+            expect(res).toHaveProperty('data', true);
+        }).catch(err => {
+            // console.log(err)
+        });
+    });
 });
