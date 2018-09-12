d9a4e41bf92bafe583410df008c6a5de8b5d5c5b
Gilles <platteeuw.gilles@gmail.com>
2018-09-12 12:29:35 +0200

    Build enhancement fixes #50

--- lib-esm/scripts/core/auth/AuthModel.d.ts
+++
@@ -1,9 +0,0 @@
-import { T1CLibException } from '../exceptions/CoreExceptions';
-export interface AbstractAuth {
-    getJWT(callback?: (error: T1CLibException, data: JWTResponse) => void): Promise<JWTResponse>;
-    refreshJWT(currentJWT: string, callback?: (error: T1CLibException, data: JWTResponse) => void): Promise<JWTResponse>;
-}
-export declare class JWTResponse {
-    token: string;
-    constructor(token: string);
-}
--- lib-esm/scripts/util/ActivatedContainerUtil.d.ts
+++
@@ -1,8 +0,0 @@
-import { T1CContainer, T1CContainerid } from '../core/service/CoreModel';
-import { GCLConfig } from '../core/GCLConfig';
-export declare class ActivatedContainerUtil {
-    constructor();
-    static getContainerFor(cfg: GCLConfig, containerName: string): string;
-    static getSortedProvidedContainers(containers: T1CContainerid[]): Map<string, string[]>;
-    static getSortedContainers(containers: T1CContainer[]): Map<string, string[]>;
-}
